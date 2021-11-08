7790bf3eb27d89e7331d7c56f9df39ba643dfcba
Justyna Kuchta <justyna.kuchta@gmail.com>
2021-11-08 12:32:53 -0500

    added info about the jwt token

--- jwt_tokens.py
+++ jwt_tokens.py
@@ -1,2 +1,3 @@
 # __all__ = ['jwt']
+# last updated on Friday, November 6th, 2021
 jwt = "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJ1c2VybmFtZSI6ImJyYmF0dEBobnRiLmNvbSIsImV4cCI6MTY0MTc2MTQxNiwiZW1haWwiOiJicmJhdHRAaG50Yi5jb20ifQ.7onHevRWxS7RYguz6TKn22Il_shNIA_uq2S9Zafl_Sc"
